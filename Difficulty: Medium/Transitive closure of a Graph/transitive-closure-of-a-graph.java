d26d20039646d6e2c618379b9ed0d0f995a52121
Deepika Surisetti <127041514+Surisettideepika999@users.noreply.github.com>
2024-07-02 15:34:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Transitive closure of a Graph/transitive-closure-of-a-graph.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int N = Integer.parseInt(in.readLine());
+            int graph[][] = new int[N][N];
+            
+            for(int i = 0;i < N;i++)
+                {String a[] = in.readLine().trim().split("\\s+");
+                for(int j=0;j<N;j++)
+                graph[i][j] = Integer.parseInt(a[j]);}
+            
+            Solution ob = new Solution();
+            ArrayList<ArrayList<Integer>> ans = ob.transitiveClosure(N, graph);
+            for(int i = 0;i < N;i++)
+               { for(int j = 0;j < ans.get(i).size();j++)
+                    System.out.print(ans.get(i).get(j) + " ");
+            System.out.println();}
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static ArrayList<ArrayList<Integer>> transitiveClosure(int N, int graph[][])
+    {
+        // code here
+          for(int i=0;i<N;i++)
+        graph[i][i]=1;
+        for(int k=0;k<N;k++){
+            for(int i=0;i<N;i++){
+                for(int j=0;j<N;j++){
+                    if(graph[i][k]==1 && graph[k][j]==1)
+                    graph[i][j]=1;
+                }
+            }
+        }
+        ArrayList<ArrayList<Integer>> a=new ArrayList<ArrayList<Integer>>();
+        for(int i=0;i<N;i++){
+            ArrayList<Integer> t=new ArrayList<>();
+            for(int j=0;j<N;j++){
+                t.add(graph[i][j]);
+            }
+            a.add(t);
+        }
+        return a;
+    }
+}
