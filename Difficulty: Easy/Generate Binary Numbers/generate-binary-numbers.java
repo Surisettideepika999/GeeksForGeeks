f7f43f4e501997ec906e86b23d4ac7d5fc7e8b29
Deepika Surisetti <127041514+Surisettideepika999@users.noreply.github.com>
2024-07-08 14:13:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Generate Binary Numbers/generate-binary-numbers.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class GFG
+{
+    public static void main(String args[])
+    {
+        //Taking input using class Scanner 
+        Scanner sc = new Scanner(System.in);
+        
+        //Taking the number of testcases
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            //taking the range
+            int n = sc.nextInt();
+            
+            //creating an ArrayList
+            ArrayList<String> res = new ArrayList<String>();
+            
+            //calling the generate method of class solve
+            //and storing the result in an ArrayList
+            res = new solve().generate(n);
+            
+            //printing all the elements of the ArrayList
+            for(int i = 0;i<res.size();i++)
+                System.out.print(res.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+
+class solve{
+    
+    //Function to generate binary numbers from 1 to N using a queue.
+    static ArrayList<String> generate(int N)
+    {
+        // Your code here
+        Queue<String> q=new LinkedList<>();
+        ArrayList<String> a=new ArrayList<>();
+        q.add("1");
+        while(a.size()<N){
+            String s=q.poll();
+            a.add(s);
+            q.add(s+'0');
+            q.add(s+'1');
+        }
+        return a;
+    }
+    
+}
